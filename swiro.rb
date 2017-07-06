bc01762f56633e0297c70a1f0d556fac109f3ad7
taku-k <taakuu19@gmail.com>
2017-07-06 12:18:33 +0900

    Add swiro.rb

---
+++ swiro.rb
@@ -0,0 +1,21 @@
+class Swiro < Formula
+  swiro_version = '0.2.2'
+  homepage 'https://github.com/taku-k/swiro'
+  version swiro_version
+
+  if Hardware.is_64_bit?
+    url "https://github.com/taku-k/swiro/releases/download/v#{swiro_version}/swiro_darwin_amd64"
+    sha256 'f6bcba7c577cb425554bd28a6ffd3dbcefdfe73bdbf823a5ef3fa0856d11294c'
+  else
+    url "https://github.com/taku-k/swiro/releases/download/v#{swiro_version}/swiro_darwin_386"
+    sha256 '09e209ee173e6dea83e96da228dd71086a50faf813a94ef0a6b64bb146aa1043'
+  end
+
+  def install
+    bin.install 'swiro'
+  end
+
+  test do
+    system 'swiro', '--version'
+  end
+end
