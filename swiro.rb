cc4047f584b730f43ee1fb80f15daa0430acb024
Takuya Kuwahara <taakuu19@gmail.com>
2018-01-07 02:43:18 +0900

    release v0.2.4

--- swiro.rb
+++ swiro.rb
@@ -1,14 +1,14 @@
 class Swiro < Formula
-  swiro_version = '0.2.2'
+  swiro_version = '0.2.4'
   homepage 'https://github.com/taku-k/swiro'
   version swiro_version
 
   if Hardware::CPU.is_64_bit?
     url "https://github.com/taku-k/swiro/releases/download/v#{swiro_version}/swiro_darwin_amd64"
-    sha256 'f6bcba7c577cb425554bd28a6ffd3dbcefdfe73bdbf823a5ef3fa0856d11294c'
+    sha256 '2c9f3bd00b402412f3e1209205e2a97d3a9ade2f80e5c7168cabb4463aa90db4'
   else
     url "https://github.com/taku-k/swiro/releases/download/v#{swiro_version}/swiro_darwin_386"
-    sha256 '09e209ee173e6dea83e96da228dd71086a50faf813a94ef0a6b64bb146aa1043'
+    sha256 'cff14bacfbad7383a79e6248d1ab187560deffb0a14b9a4ac408ca2aba884424'
   end
 
   def install
